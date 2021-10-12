5e3745c6168cdb5545e2cf288888ee3ed6aa67da
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-10-13 02:43:28 +0900

    test: ROUTE - 메인 & 히스토리 타입 변경

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,39 +1,39 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import App from './app';
-import { createMemoryHistory } from 'history'
+import { createBrowserHistory } from 'history';
 import { render, screen } from '@testing-library/react';
 
 describe('App', () => {
     test('메인화면', () => {
-        const history = createMemoryHistory();
+        const history = createBrowserHistory();
         render(
             <Router history={history}>
                 <App />
             </Router>
         );
-        expect(screen.getByText('Main')).toBeInTheDocument();
+        expect(screen.getByText('스몰퀴즈')).toBeInTheDocument();
+        expect(screen.getByText('퀴즈풀기')).toBeInTheDocument();
+        expect(screen.getByText('오답노트')).toBeInTheDocument();
     });
-    test('퀴즈', () => {
-        const history = createMemoryHistory();
+    test('퀴즈풀기', () => {
+        const history = createBrowserHistory();
         history.push('/quiz');
         render(
             <Router history={history}>
                 <App />
             </Router>
         );
-
         expect(screen.getByText('퀴즈')).toBeInTheDocument();
     });
     test('오답노트', () => {
-        const history = createMemoryHistory();
-        
-        history.push('/wrong')
+        const history = createBrowserHistory();
+        history.push('/wrong');
         render(
             <Router history={history}>
                 <App />
             </Router>
-        );;
+        );
         expect(screen.getByText('오답노트')).toBeInTheDocument();
     });
 });

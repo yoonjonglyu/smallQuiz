8cb796a7cc81b8aec6af981aa371a4c39d71daaa
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-10-13 01:44:30 +0900

    feat: MAIN PAGE - 기본 구성

--- src/pages/main.stories.tsx
+++ src/pages/main.stories.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Main from './main';
 
 export default {
     title: '스몰퀴즈/페이지/Main',
-    component: Main
+    component: Main,
+    decorators: [(Story) => <Router><Story /></Router>]
 } as ComponentMeta<typeof Main>
 
 const template: ComponentStory<typeof Main> = (props) => <Main {...props} />;
--- src/pages/main.test.tsx
+++ src/pages/main.test.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Main from './main';
 
 import { render, screen } from '@testing-library/react';
 
 
-
 describe('Main 컴포넌트', () => {
     test('Main 컴포넌트 렌더링', () => {
-        render(<Main />);
-        expect(screen.getByText("Main")).toBeInTheDocument();
+        render(
+            <Router>
+                <Main />
+            </Router>
+        );
+        expect(screen.getByText("스몰퀴즈")).toBeInTheDocument();
+        expect(screen.getByText("퀴즈풀기")).toBeInTheDocument();
+        expect(screen.getByText("오답노트")).toBeInTheDocument();
     });
 });

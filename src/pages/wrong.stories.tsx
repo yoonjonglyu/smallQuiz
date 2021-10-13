03e8647e296892237d33fa425d1f7746158e233b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-10-13 11:45:50 +0900

    feat: WRONG PAGE - 오답 데이터 바인딩 & 오답 없음 알림

--- src/pages/wrong.stories.tsx
+++ src/pages/wrong.stories.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import Wrong from './wrong';
 
 export default {
     title: "스몰퀴즈/페이지/Wrong",
-    component: Wrong
+    component: Wrong,
+    decorators: [(Story) => <Router><Story /></Router>]
 } as ComponentMeta<typeof Wrong>
 
 const template: ComponentStory<typeof Wrong> = (props) => <Wrong {...props} />;
 
+const quiz = {
+    question: 'This Greek mythological figure is the god\/goddess of battle strategy (among other things)',
+    correct_answer: "Athena"
+};
+
 export const Basic = template.bind({});
 Basic.args = {
-
+    wrongs: new Array(5).fill(quiz)
 };
--- src/pages/wrong.test.tsx
+++ src/pages/wrong.test.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Wrong from './wrong';
 
 import { render, screen } from '@testing-library/react';
 
 describe('Wrong 컴포넌트', () => {
+    const quiz = {
+        question: 'This Greek mythological figure is the god\/goddess of battle strategy (among other things)',
+        correct_answer: "Athena"
+    };
+
     test('wrong 컴포넌트 렌더링', () => {
-        render(<Wrong />);
-        expect(screen.getByRole('heading')).toHaveTextContent('오답 노트');
+        render(
+            <Router>
+                <Wrong wrongs={new Array(5).fill(quiz)} />
+            </Router>
+        );
+        expect(screen.getAllByRole('heading')[0]).toHaveTextContent('오답 노트');
+        expect(screen.getByRole('main')).toHaveTextContent(`문제: ${quiz.question}`);
+        expect(screen.getByRole('main')).toHaveTextContent(`정답: ${quiz.correct_answer}`);
+        expect(screen.getByRole('link')).toHaveTextContent('처음으로');
+    });
+    test('오답이 없을 시', () => {
+        render(
+            <Router>
+                <Wrong wrongs={[]} />
+            </Router>
+        );
+        expect(screen.getAllByRole('heading')[0]).toHaveTextContent('오답 노트');
+        expect(screen.getByText('틀린 문제가 없습니다.')).toBeInTheDocument();
+        expect(screen.getByRole('main')).not.toHaveTextContent('문제:');
+        expect(screen.getByRole('main')).not.toHaveTextContent('정답:');
+        expect(screen.getByRole('link')).toHaveTextContent('처음으로');
     });
 });

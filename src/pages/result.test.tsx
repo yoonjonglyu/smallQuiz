d0e03edb9a1ea9dc4d13936df99232a4372beea1
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-10-13 16:10:48 +0900

    feat: RESULT PAGE - 다시풀기 & 오답노트 이동

--- src/pages/result.test.tsx
+++ src/pages/result.test.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import Result from './result';
 
 import { render, screen } from '@testing-library/react';
@@ -11,13 +13,19 @@ describe('Result 컴포넌트', () => {
     };
 
     test('Result 컴포넌트 렌더링', () => {
-        render(<Result statis={statis} />);
+        render(
+            <RecoilRoot>
+                <Router>
+                    <Result statis={statis} />
+                </Router>
+            </RecoilRoot>
+        );
         expect(screen.getByRole('heading')).toHaveTextContent('풀이 결과');
         expect(screen.getByRole('main'))
-        .toHaveTextContent(`소요시간: ${(statis.time - statis.time % 60) / 60} 분 ${statis.time % 60} 초`);
+            .toHaveTextContent(`소요시간: ${(statis.time - statis.time % 60) / 60} 분 ${statis.time % 60} 초`);
         expect(screen.getByRole('main')).toHaveTextContent(`정답 갯수: ${statis.answer} 개`);
         expect(screen.getByRole('main')).toHaveTextContent(`오답 갯수: ${statis.wrong} 개`);
-        expect(screen.getByRole('main')).toHaveTextContent('다시 풀기');
-        expect(screen.getByRole('main')).toHaveTextContent('오답 노트');
+        expect(screen.getAllByRole('link')[0]).toHaveTextContent('다시 풀기');
+        expect(screen.getAllByRole('link')[1]).toHaveTextContent('오답 노트');
     });
 });

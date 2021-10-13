ee363f1d7af1201e5dab6d5d4c6b4e04e28dde1a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-10-13 11:18:12 +0900

    feat: RESULT PAGE - BIND PROPS STATIS

--- src/pages/result.stories.tsx
+++ src/pages/result.stories.tsx
@@ -13,5 +13,9 @@ const template: ComponentStory<typeof Result> = (props) => <Result {...props} />
 
 export const Basic = template.bind({});
 Basic.args = {
-
+    statis: {
+        time: 388,
+        answer: 8,
+        wrong: 2,
+    }
 };
--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 
 import Layout from '../components/layout/layout';
 
-const Result: React.FC = () => {
+interface ResultProps {
+    statis: {
+        time: number
+        answer: number
+        wrong: number
+    };
+}
+const Result: React.FC<ResultProps> = (props) => {
+    const {
+        statis
+    } = props;
+
+
     return (
         <Layout header="풀이 결과">
             <ul>
-                <li>소요시간: </li>
-                <li>정답 갯수: </li>
-                <li>오답 갯수: </li>
+                <li>소요시간: {(statis.time - statis.time % 60) / 60} 분 {statis.time % 60} 초</li>
+                <li>정답 갯수: {statis.answer} 개</li>
+                <li>오답 갯수: {statis.wrong} 개</li>
             </ul>
             <ul>
                 <li><button type="button">다시 풀기</button></li>
